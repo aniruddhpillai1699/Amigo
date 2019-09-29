7f4bfb167b17f7b84f754a777e293121e763c773
aniruddhpillai1699 <aniruddhpillai64@gmail.com>
2019-09-29 09:50:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amigo/ChatBar.java
@@ -0,0 +1,233 @@
+package com.example.amigo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+import ai.api.AIServiceContext;
+import ai.api.AIServiceContextBuilder;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIDataService;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+
+
+public class ChatBar extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int USER = 10001;
+    private static final int BOT = 10002;
+
+    private String uuid = UUID.randomUUID().toString();
+    private LinearLayout chatLayout;
+    private EditText queryEditText;
+
+    // Android client
+    private AIRequest aiRequest;
+    private AIDataService aiDataService;
+    private AIServiceContext customAIServiceContext;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_bar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        final ScrollView scrollview = findViewById(R.id.chatScrollView);
+        scrollview.post(() -> scrollview.fullScroll(ScrollView.FOCUS_DOWN));
+
+        chatLayout = findViewById(R.id.chatLayout);
+
+        ImageView sendBtn = findViewById(R.id.sendBtn);
+        sendBtn.setOnClickListener(this::sendMessage);
+
+        queryEditText = findViewById(R.id.queryEditText);
+        queryEditText.setOnKeyListener((view, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                switch (keyCode) {
+                    case KeyEvent.KEYCODE_DPAD_CENTER:
+                    case KeyEvent.KEYCODE_ENTER:
+                        sendMessage(sendBtn);
+                        return true;
+                    default:
+                        break;
+                }
+            }
+            return false;
+        });
+
+        // Android client
+        initChatbot();
+
+    }
+    private void initChatbot() {
+        final AIConfiguration config = new AIConfiguration("73a05c9a4dca4708bfc5c07041051240",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+        aiDataService = new AIDataService(this, config);
+        customAIServiceContext = AIServiceContextBuilder.buildFromSessionId(uuid);// helps to create new session whenever app restarts
+        aiRequest = new AIRequest();
+    }
+    private void sendMessage(View view) {
+        String msg = queryEditText.getText().toString();
+        if (msg.trim().isEmpty()) {
+            Toast.makeText(ChatBar.this, "Please enter your query!", Toast.LENGTH_LONG).show();
+        } else {
+            showTextView(msg, USER);
+            queryEditText.setText("");
+            // Android client
+            aiRequest.setQuery(msg);
+            RequestTask requestTask = new RequestTask(ChatBar.this, aiDataService, customAIServiceContext);
+            requestTask.execute(aiRequest);
+
+        }
+    }
+    public void callback(AIResponse aiResponse) {
+        if (aiResponse != null) {
+            // process aiResponse here
+            String botReply = aiResponse.getResult().getFulfillment().getSpeech();
+            Log.d(TAG, "Bot Reply: " + botReply);
+            showTextView(botReply, BOT);
+        } else {
+            Log.d(TAG, "Bot Reply: Null");
+            showTextView("There was some communication issue. Please Try again!", BOT);
+        }
+    }
+    private void showTextView(String message, int type) {
+        FrameLayout layout;
+        switch (type) {
+            case USER:
+                layout = getUserLayout();
+                break;
+            case BOT:
+                layout = getBotLayout();
+                break;
+            default:
+                layout = getBotLayout();
+                break;
+        }
+        layout.setFocusableInTouchMode(true);
+        chatLayout.addView(layout); // move focus to text view to automatically make it scroll up if softfocus
+        TextView tv = layout.findViewById(R.id.chatMsg);
+        tv.setText(message);
+        layout.requestFocus();
+        queryEditText.requestFocus(); // change focus back to edit text to continue typing
+    }
+
+
+    FrameLayout getUserLayout() {
+        LayoutInflater inflater = LayoutInflater.from(ChatBar.this);
+        return (FrameLayout) inflater.inflate(R.layout.user_msg_layout, null);
+    }
+
+
+    FrameLayout getBotLayout() {
+        LayoutInflater inflater = LayoutInflater.from(ChatBar.this);
+        return (FrameLayout) inflater.inflate(R.layout.bot_msg_layout, null);
+    }
+
+
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.chat_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        // fragment = new MyAccountFragment();
+        if (id == R.id.nav_MyAccount) {
+            intent = new Intent(this, MyAccount.class);
+        }
+        else if (id == R.id.nav_About) {
+            intent = new Intent(this,About.class);
+
+        }
+
+        if(fragment!=null) {
+            FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame,fragment);
+            ft.commit();
+        } else {
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+}
